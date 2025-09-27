dc208561ab637603fea1e78cac24549467851dea
JassoProg <ernestojassof@gmail.com>
2025-09-27 11:26:19 -0600

    Eliminar carpeta react-vite-pwa innecesaria - Fix para deploy en Netlify

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.fgvrcvvs5ho"
+    "revision": "0.7mbih110co8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
